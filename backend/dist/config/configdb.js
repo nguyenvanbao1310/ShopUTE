97654a68ebc6580b4f45d341b6a73bfa75061ce6
HuyIT3 <hoanghuy010104@gmail.com>
2025-08-28 19:03:33 +0700

    commit giải quyết conflict

--- backend/dist/config/configdb.js
+++ backend/dist/config/configdb.js
@@ -1,20 +1,25 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.connectDB = void 0;
 const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize("shopute", "root", "751676Kh@", {
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const DB_PASS = process.env.DB_PASS;
+const sequelize = new sequelize_1.Sequelize("shopute", "root", DB_PASS, {
     host: "localhost",
     dialect: "mysql",
     logging: false,
 });
 const connectDB = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log("✅ Connection has been established successfully.");
-    }
-    catch (error) {
-        console.error("❌ Unable to connect to the database:", error);
-    }
+  try {
+    await sequelize.authenticate();
+    console.log("✅ Connection has been established successfully.");
+  } catch (error) {
+    console.error("❌ Unable to connect to the database:", error);
+  }
 };
 exports.connectDB = connectDB;
 // 👉 export cả instance sequelize để models, migration dùng
--- frontend/shopute/src/App.tsx
+++ frontend/shopute/src/App.tsx
@@ -2,8 +2,17 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <Login />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 export default App;
